c1cc7983be59105817eb4dddb9f35e4ef85891e9
DayakerZ <dayakerreddy.kandala@gmail.com>
2022-05-16 09:56:41 +0530

    Excepton controller added

---
+++ src/main/java/com/assignment/root/assignment/exceptions/BookExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.assignment.root.assignment.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+public class BookExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<BookErrorResponse> handleException(BookNotFoudException exc) {
+
+
+        BookErrorResponse error = new BookErrorResponse(
+                HttpStatus.NOT_FOUND.value(),
+                exc.getMessage(),
+                System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+}
--- src/main/java/com/assignment/root/assignment/exceptions/BookNotFoudException.java
+++ src/main/java/com/assignment/root/assignment/exceptions/BookNotFoudException.java
@@ -1,6 +1,6 @@
 package com.assignment.root.assignment.exceptions;
 
-public class BookNotFoudException extends Exception{
+public class BookNotFoudException extends RuntimeException{
 
     public BookNotFoudException(String s)
     {

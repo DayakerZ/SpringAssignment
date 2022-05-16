84cd9f5254b225adf4a87db6159fc9406da288c7
DayakerZ <dayakerreddy.kandala@gmail.com>
2022-05-16 10:06:43 +0530

    code smells removed

--- src/test/java/com/assignment/root/assignment/service/BookServiceImplTest.java
+++ src/test/java/com/assignment/root/assignment/service/BookServiceImplTest.java
@@ -18,7 +18,7 @@ import java.util.Optional;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class BookServiceImplTest {
+class BookServiceImplTest {
 
 
     @Mock
